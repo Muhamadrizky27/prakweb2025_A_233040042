f3d1f9c0b25e4602c07cfb640492551662e75c0e
Muhamadrizky27 <rizkyers80@gmail.com>
2025-11-11 10:14:53 +0700

    crud

---
+++ tugas/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define("BASEURL", "http://localhost:8080/prakweb2025_A_233040042/pertemuan2/tugas/TemplateMVC/public/");
+
+// DATANASE
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2');
